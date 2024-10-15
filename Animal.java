e86b967330deec8cf2f7e32407d6d3b884b8e5d2
shruti2193 <shrutidixit2193@gmail.com>
2024-10-15 11:09:41 -0700

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -1,49 +1,99 @@
+package shruti.zoo.com;
+
 import java.util.Date;
 
 public class Animal {
-    // creae a static int that keep track of the number of the animals created.
-    static int numofanimals = 0;
-    // ceate a constructor for our new animal objects
-    public Animal () {
-        numofanimals++;
+    // Create a static int that keep track of the number of animals created.
+    static int numOfAnimals = 0;
 
+    // Create a constructor for our new Animal objects
+    public Animal() {
+        numOfAnimals++;
     }
-    // create a constructor that accepts a name
-    public Animal(String aName) {
+
+    // Create a constructor that will accept all fields as arguments.
+    public Animal(String sex, int age, int weight, String animalName,
+                  String animalID, String animalBirthDate, String animalColor,
+                  String animalOrigin) {
+        this.sex = sex;
+        this.age = age;
+        this.weight = weight;
+        this.animalName = animalName;
+        this.animalID = animalID;
+        this.animalBirthDate = animalBirthDate;
+        this.animalColor = animalColor;
+        this.animalOrigin = animalOrigin;
     }
-}
-    //Create a few fields
-    // Sex will be male or female
-    private String sex = "";
-    private int age = 0;
+
+
+    // Create all attributes (fields) needed for our midterm program.
+    // sex will be 'male' or 'female'
+    private String sex;
+    // age will be in years
+    private int age;
     // weight will be in pounds
-    private int weight = 0;
+    private int weight;
+    // name will be a String;
+    private String animalName;
+    // animalID
+    private String animalID;
+    // animalBirthDate is going to be a string here (in this class)
+    private String animalBirthDate;
+    // animalColor is a String
+    private String animalColor;
+    // origin will be a string like: "from Friguia Park, Tunisia"
+    private String animalOrigin;
+
+
+
+
     // Create getters and setters
-    // unique id for each animal
-    private String animalID="";
-    // animal name come from a text file called animalnames.txt
-    private String animalnme = "";
-    private Date animalbirthdate;
 
+    public String getAnimalOrigin() {
+        return animalOrigin;
+    }
+    public void setAnimalOrigin(String animalOrigin) {
+        this.animalOrigin = animalOrigin;
+    }
 
+    public String getAnimalColor() {
+        return animalColor;
+    }
+    public void setAnimalColor(String animalColor) {
+        this.animalColor = animalColor;
+    }
 
+    public String getAnimalBirthDate() {
+        return animalBirthDate;
+    }
+    public void setAnimalBirthDate(String animalBirthDate) {
+        this.animalBirthDate = animalBirthDate;
+    }
 
+    public String getAnimalID() {
+        return animalID;
+    }
+    public void setAnimalID(String animalID) {
+        this.animalID = animalID;
+    }
 
-
+    public String getAnimalName() {
+        return animalName;
+    }
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
 
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
-
     }
 
     public String getSex() {
         return sex;
     }
-
     public void setSex(String sex) {
         this.sex = sex;
     }
@@ -55,12 +105,4 @@ public class Animal {
     public void setWeight(int weight) {
         this.weight = weight;
     }
-    public String getAnimalID(){return animalID;}
-    public void setAnimalID(String animalID) {this.animalID = animalID;}
-
-      public String getanimalNAME(){return animalnme;}
-    public void setAnimalnme  (String animalnme) {this.animalnme;}
-public String getanimalbirthday (){return animalbirthdate;}
-
-
-
+}
--- Hyena.java
+++ Hyena.java
@@ -1,5 +1,13 @@
-package shruti.zoo.com
+package shruti.zoo.com;
 
-public class Hyena extends shruti.zoo.com {
-    static int numofhyenas = 
+public class Hyena extends shruti.zoo.com.Animal {
+    // Create a static int that keep track of the number of hyenas created.
+    static int numOfHyenas = 0;
+
+    // Create a constructor.
+    public Hyena(){
+        super();
+        numOfHyenas++;
+    }
 }
+
